28628246766898dc84f070ddb480aa5c2dc695be
Gaston Garcia Juri <gastongarciajuri@gmail.com>
2023-11-23 19:00:46 -0300

    btns, color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-scroll'
-import logo from '../images/logo1.png'
-
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import logo from '../images/logo1.png';
+import SolKeyIcon from './SolKeyIcon';
 
 const NavBar = () => {
+  // Estado para realizar un seguimiento del enlace activo
+  const [activeLink, setActiveLink] = useState('main');
+
+  // Función para manejar el clic en un enlace
+  const handleLinkClick = (to) => {
+    setActiveLink(to);
+  };
+
   return (
     <div>
       <nav>
-        <Link to='main' className='logo' smooth={true} duration={2000} >
+        <Link to='main' className='logo' smooth={true} duration={2000}>
           <img src={logo} alt="logo" />
         </Link>
         <input className='menu-btn' type='checkbox' id='menu-btn' />
@@ -15,15 +23,51 @@ const NavBar = () => {
           <span className='nav-icon'></span>
         </label>
         <ul className='menu'>
-          <li><Link to='main' className='active' smooth={true} duration={1000} >ABOUT</Link></li>
-          <li><Link to='features' smooth={true} duration={1000}>TECH SKILLS</Link></li>
-          <li><Link to='services' smooth={true} duration={1000}>PROYECTOS</Link></li>
-          <li><Link to='#'>CONTACTO</Link></li>
+          <li>
+            <Link
+              to='main'
+              className={activeLink === 'main' ? 'active' : ''}
+              smooth={true}
+              duration={1000}
+              onClick={() => handleLinkClick('main')}
+            >
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='features'
+              className={activeLink === 'features' ? 'active' : ''}
+              smooth={true}
+              duration={1000}
+              onClick={() => handleLinkClick('features')}
+            >
+              TECH SKILLS
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='services'
+              className={activeLink === 'services' ? 'active' : ''}
+              smooth={true}
+              duration={1000}
+              onClick={() => handleLinkClick('services')}
+            >
+              PROYECTOS
+            </Link>
+          </li>
+          <li>
+            <Link to='#' className={activeLink === 'contact' ? 'active' : ''} onClick={() => handleLinkClick('contact')}>
+              CONTACTO
+            </Link>
+          </li>
         </ul>
-        <Link to='#' className='hey'>To App</Link>
+        <Link to='#' className='hey'>
+          To App
+        </Link>
       </nav>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
