c8dc6f323ca4187c6bc49313e91236f120ef5a27
Gaston Garcia Juri <gastongarciajuri@gmail.com>
2023-11-24 17:47:04 -0300

    colors

---
+++ src/assets/data.js
@@ -0,0 +1,4 @@
+export const text = {
+    serendipia: "Serendipia es una aplicacion de React, que simula un e-commerce de una tienda digital de libros. Construida por un grupo de 6 para el proyecto final del bootcamp 'Soy Henry'. Alunas de las tecnologias usadas son Redux, Node, Express, Sequelize, PostgreSql, Css Modules.",
+    countries:"Countries es un proyecto individual desarrollado para una etapa de examen del bootcamp 'Soy Henry'. Esta aplicacion hace una request a una api externa para traer paises y algunos otros datos de interes, ofrece un buscador y botones para filtrar y ordenar de distintas maneras. La app nos permite asignarle actividades turisticas a los distintos paises para despues poder filtrarlos por estas mismas."
+}
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FaGithub, FaGlobe } from 'react-icons/fa';
 
 const Box = (props) => {
   return (
     <div className='s-box'>
-        <div className='s-b-img'>
-            <img src={props.image} alt={props.alte} />
-        </div> 
-        <div className='s-b-text'>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Enim rerum ipsam, doloribus nemo amet iste illum alias voluptate beatae! 
-                Quia, aut! Eos placeat dolorem nam?</p>
-                <a href='#' className='cv-btn'>{props.button}</a>
+      <div className='s-b-img'>
+        <video src={props.video}></video>
+      </div>
+      <div className='s-b-text'>
+        <p>{props.text}</p>
+        <div className='b-container'>
+          <a href={props.deployLink} className='cv-btn' target='_blank' rel='noopener noreferrer'>
+            <FaGlobe />
+            {props.button1}
+          </a>
+          <a href={props.repoLink} className='cv-btn' target='_blank' rel='noopener noreferrer'>
+            <FaGithub />
+            Repo
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box;
